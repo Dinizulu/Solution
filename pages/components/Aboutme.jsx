617494de3132437b36aea7e64a60432662c9fabb
Dinizulu <j.dinizulu@gmail.com>
2022-06-20 03:27:23 +0200

    Commit of the app

---
+++ pages/components/Aboutme.jsx
@@ -0,0 +1,15 @@
+const Aboutme = () => {
+    return(
+        <>
+        A warm welcome to my miniprofile presentation my name is Jabulane Dinizulu.
+        I am a recent Information Technology graduate who currently hold an honors degree in the field.
+        I did my highschool learning at Relebogile Secondary school in Khutsong South and higher education at North-West university
+        which is located at Vanderbijlpark. I aspire to be in a siniour level on various software develpment roles within
+        one of the top sofware development companies as yourselves. I am hard working detail oriented individual with a hungry mind to learn and explore
+        all the possibilities within the sector of software development and information technology. Currenlty I am looking for an entry level
+        software development role with vast and open learning opportunities which will enable me to grow and meet all the reaquired qualities 
+        traits in the software development sector and information technology as a whole.
+        </>
+    )
+}
+export default Aboutme;
---
+++ pages/components/Why.jsx
@@ -0,0 +1,18 @@
+const Why = () => {
+    return(
+        <>
+        SovTech is a high reputable company in providing software solutions to clients and stakeholders with a
+        rating of 4 out of 5 stars. This alone to me is inspiring as I know that there will be enough room for 
+        me to improve due to ratings show clearly that my journey as newbie in the organization will be thrusted
+        with high quality learning and access to resources wont be a problem as the organization caters for that.
+         This for me will be a golden opportunity as I know that the organization through the program will give me
+         a personal mentor, which this will be helpful and a stepping stone towards grasping required competencies
+          and performance capabilities. The organization is already in the stream of building technologies using the 
+          latest norms and tools, thus this need a mind that is hungry for knowledge with the aim to excell with success.
+           I believe I will be the best suit for such due to what I love most is exploring and creating new solution through
+            experimental testing.
+
+        </>
+    )
+}
+export default Why;
